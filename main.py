01ad8b7d34d2e5f37d36d5875cfe2986678f1c23
Karan-NT <98152815+Karan-NT@users.noreply.github.com>
2022-01-29 23:09:22 +0330

    first fully functional version of the project

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from multiprocessing import connection
+from email.mime.text import MIMEText
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import sqlite3
+import smtplib
 import time
 
+username = input('ENTER USERNAME: ')
+password = input('ENTER PASSWORD: ')
+
+recipient = input('\nENTER RECIPIENT: ')
+
 dataURL = input('\nENTER URL: ')
-print('\nRETRIEVING HTML...')
 
-conn = sqlite3.connect('database.sqlite')
-cursor = conn.cursor()
+def getPage():
+    print('\nRETRIEVING HTML...')
 
-driver = webdriver.Chrome()
-driver.get(dataURL)
-page = driver.page_source
+    driver = webdriver.Chrome()
+    driver.get(dataURL)
+    page = driver.page_source
+    data = BeautifulSoup(page, 'html.parser')
 
-data = BeautifulSoup(page, 'html.parser')
-print('\nRETRIEVAL SUCCESSFUL\n\nRETRIEVING PRODUCTS...')
+    driver.quit()
+    print('\nRETRIEVAL SUCCESSFUL')
 
-driver.quit()
+    return data
+
+newData = getPage()
+oldData = newData
 
 li = list()
 class product:
@@ -34,39 +43,93 @@ class product:
                 targetClass in child.get('class')
             ): return child.text
 
-for tag in data.find_all('div'):
-    try: tag.get('class')
-    except: continue
+def updateDatabase():
+    conn = sqlite3.connect('database.sqlite')
+    cursor = conn.cursor()
 
-    if ( (type(tag.get('class'))) == type(li) and 
-        'productItemName_3IZ3c' in tag.get('class')
-    ): pass
-    else: continue
+    print('\nRETRIEVING PRODUCTS...')
 
-    obj = product()
-    obj.name = tag.text
+    for tag in newData.find_all('div'):
+        try: tag.get('class')
+        except: continue
 
-    obj.availability = product.retrieveAttribute(
-        tag, 
-        'span', 
-        'container_1DAvI'
-    )
+        if ( (type(tag.get('class'))) == type(li) and 
+            'productItemName_3IZ3c' in tag.get('class')
+        ): pass
+        else: continue
 
-    obj.price = product.retrieveAttribute(
-        tag, 
-        'span', 
-        'screenReaderOnly_3anTj'
-    )
+        obj = product()
+        obj.name = tag.text
 
-    print(obj.name, ' - ', obj.availability, ' - ', obj.price,)
+        obj.availability = product.retrieveAttribute(
+            tag, 
+            'span', 
+            'container_1DAvI'
+        )
 
-    cursor.execute('INSERT INTO Status (availability) VALUES (?)', (obj.availability, ))
-    cursor.execute('SELECT id FROM Status WHERE availability = ?', (obj.availability, ))
-    status_id = cursor.fetchone()[0]
+        obj.price = product.retrieveAttribute(
+            tag, 
+            'span', 
+            'screenReaderOnly_3anTj'
+        )
 
-    cursor.execute('INSERT INTO Product (name, price, status_id) VALUES (?, ?, ?)', (obj.name, obj.price, status_id))
+        print(obj.name, ' - ', obj.availability, ' - ', obj.price,)
 
-cursor.close()
-conn.commit()
+        cursor.execute('''INSERT OR IGNORE INTO 
+            Status (availability) VALUES (?)''', 
+            (obj.availability, )
+        )
+        cursor.execute('''SELECT id FROM Status 
+            WHERE availability = ?''', 
+            (obj.availability, )
+        )
+        status_id = cursor.fetchone()[0]
 
-print('\nRETRIEVAL SUCCESSFUL')
+        cursor.execute('''INSERT OR REPLACE INTO Product 
+            (name, price, status_id) VALUES (?, ?, ?)''', 
+            (obj.name, obj.price, status_id)
+        )
+
+    print('\nRETRIEVAL SUCCESSFUL')
+
+    cursor.close()
+    conn.commit()
+
+def send():
+    conn = sqlite3.connect('Database.sqlite')
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM Product JOIN Status 
+        ON Product.status_id = Status.id''')
+
+    content = str(dataURL)
+    for row in cursor.fetchall():
+        content = content + '\n' + str(row)
+
+    msg = MIMEText(content)
+    msg['Subject'] = 'Update on tracked products'
+    msg['From'] = username
+    msg['To'] = recipient
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(
+            username, 
+            [recipient], 
+            msg.as_string()
+        )
+        print('\nMAIL SENT SUCCESSFULLY')
+        server.quit()
+    except:
+        print('\nFAILED TO SEND MAIL')
+        server.quit()
+
+while True:
+    updateDatabase()
+    send()
+    while oldData == newData:
+        time.sleep(600)
+        newData = getPage()
+    oldData = newData
