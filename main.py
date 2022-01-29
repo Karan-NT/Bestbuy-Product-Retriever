941316a0cc1bc6163e51e01075827991aabcd43e
Karan-NT <98152815+Karan-NT@users.noreply.github.com>
2022-01-29 18:25:55 +0330

    finalize the basic structure + a working database
    main.py can now read the page source and retrieve and move necessary data into a .sqlite file

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+from multiprocessing import connection
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import sqlite3
 import time
 
 dataURL = input('\nENTER URL: ')
 print('\nRETRIEVING HTML...')
 
+conn = sqlite3.connect('database.sqlite')
+cursor = conn.cursor()
+
 driver = webdriver.Chrome()
 driver.get(dataURL)
 page = driver.page_source
+
 data = BeautifulSoup(page, 'html.parser')
 print('\nRETRIEVAL SUCCESSFUL\n\nRETRIEVING PRODUCTS...')
+
+driver.quit()
 
 li = list()
 class product:
@@ -37,9 +45,28 @@ for tag in data.find_all('div'):
 
     obj = product()
     obj.name = tag.text
-    obj.availability = product.retrieveAttribute(tag, 'span', 'container_1DAvI')
-    obj.price = product.retrieveAttribute(tag, 'span', 'screenReaderOnly_3anTj large_3aP7Z')
-    print(obj.name, obj.availability, obj.price)
 
-driver.quit()
-#print('\nRETRIEVAL SUCCESSFUL\n\n', li)
+    obj.availability = product.retrieveAttribute(
+        tag, 
+        'span', 
+        'container_1DAvI'
+    )
+
+    obj.price = product.retrieveAttribute(
+        tag, 
+        'span', 
+        'screenReaderOnly_3anTj'
+    )
+
+    print(obj.name, ' - ', obj.availability, ' - ', obj.price,)
+
+    cursor.execute('INSERT INTO Status (availability) VALUES (?)', (obj.availability, ))
+    cursor.execute('SELECT id FROM Status WHERE availability = ?', (obj.availability, ))
+    status_id = cursor.fetchone()[0]
+
+    cursor.execute('INSERT INTO Product (name, price, status_id) VALUES (?, ?, ?)', (obj.name, obj.price, status_id))
+
+cursor.close()
+conn.commit()
+
+print('\nRETRIEVAL SUCCESSFUL')
